049b058599f8c56a0ceb9859bd83728d40959622
ahjadi <alihussainaljadi@gmail.com>
2025-04-19 20:24:52 +0300

    Initial commit

---
+++ src/main/kotlin/com/alialjadi/remedybackend/RemedyBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.alialjadi.remedybackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RemedyBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<RemedyBackendApplication>(*args)
+}
